5f303b32a2afc2d533444d91ef8def02382801da
wnsdu <wnsdu@DESKTOP-LEEJY>
2023-12-11 15:24:58 +0900

    이준영

---
+++ finalApp/src/main/java/com/fa/plus/admin/domain/adminDomain.java
@@ -0,0 +1,10 @@
+package com.fa.plus.admin.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fa.plus.admin.service.adminService;
+
+public class adminDomain {
+	@Autowired
+	private adminService service;
+}
