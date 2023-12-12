744b67aa7492fbf0350e2d1c3b74a1a5b4f0ea4a
HJ IM <74124891+hj0064@users.noreply.github.com>
2023-12-12 15:06:00 +0900

    공지사항 도메인 - 희정

---
+++ finalApp/src/main/java/com/fa/plus/domain/Notice.java
@@ -0,0 +1,93 @@
+package com.fa.plus.domain;
+
+public class Notice {
+	private long num;
+	private String subject;
+	private String content;
+	private int hitCount;
+	private int notice;
+	private int showNotice;
+	private String regDate;
+	
+	private String userId;
+	
+	private long fileNum;
+	private String saveFileName;
+	private String originalFileName;
+	private long fileSize;
+	
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public int getNotice() {
+		return notice;
+	}
+	public void setNotice(int notice) {
+		this.notice = notice;
+	}
+	public int getShowNotice() {
+		return showNotice;
+	}
+	public void setShowNotice(int showNotice) {
+		this.showNotice = showNotice;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public long getFileNum() {
+		return fileNum;
+	}
+	public void setFileNum(long fileNum) {
+		this.fileNum = fileNum;
+	}
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+	public String getOriginalFileName() {
+		return originalFileName;
+	}
+	public void setOriginalFileName(String originalFileName) {
+		this.originalFileName = originalFileName;
+	}
+	public long getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+	
+	
+}
