995dda14a79bb4f72f0c4df1ff75c20a1a6530cc
HJ IM <74124891+hj0064@users.noreply.github.com>
2023-12-17 23:07:32 +0900

    원데이플러스 도메인 - 희정

---
+++ plus/src/main/java/com/fa/plus/domain/PlusAns.java
@@ -0,0 +1,34 @@
+package com.fa.plus.domain;
+
+public class PlusAns {
+	private long ansNum;
+	private long plusNum;
+	private String userId;
+	private String userName;
+	
+	public long getAnsNum() {
+		return ansNum;
+	}
+	public void setAnsNum(long ansNum) {
+		this.ansNum = ansNum;
+	}
+	public long getPlusNum() {
+		return plusNum;
+	}
+	public void setPlusNum(long plusNum) {
+		this.plusNum = plusNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
---
+++ plus/src/main/java/com/fa/plus/domain/PlusQ.java
@@ -0,0 +1,57 @@
+package com.fa.plus.domain;
+
+public class PlusQ {
+	private long num;
+	private String subject;
+	
+	private long plusNum;
+	private String content;
+	private String qCategory;
+	
+	private long imgNum;
+	private String imgName;
+	
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public long getPlusNum() {
+		return plusNum;
+	}
+	public void setPlusNum(long plusNum) {
+		this.plusNum = plusNum;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getqCategory() {
+		return qCategory;
+	}
+	public void setqCategory(String qCategory) {
+		this.qCategory = qCategory;
+	}
+	public long getImgNum() {
+		return imgNum;
+	}
+	public void setImgNum(long imgNum) {
+		this.imgNum = imgNum;
+	}
+	public String getImgName() {
+		return imgName;
+	}
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+	
+}
