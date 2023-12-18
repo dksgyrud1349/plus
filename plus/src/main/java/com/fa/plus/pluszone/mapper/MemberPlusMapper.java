2560e854ec4cd532e2bdfd03d79a2ad98077aee7
HJ IM <74124891+hj0064@users.noreply.github.com>
2023-12-18 15:48:35 +0900

    mapper 수정 - 희정

--- plus/src/main/java/com/fa/plus/pluszone/mapper/MemberPlusMapper.java
+++ plus/src/main/java/com/fa/plus/pluszone/mapper/MemberPlusMapper.java
@@ -1,6 +1,7 @@
 package com.fa.plus.pluszone.mapper;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -13,12 +14,19 @@ public interface MemberPlusMapper {
 	
 	public long memberSeq();
 	public void insertPlus(long seq) throws SQLException;
-	public void insertPlus1(MemberPlus dto) throws SQLException;
-	public void insertPlus2(MemberPlus dto) throws SQLException;
 	public void insertPlus12(MemberPlus dto) throws SQLException;
+	
+	public void updatePlusEnabled(Map<String, Object> map) throws SQLException;
+	public void updatePlus1(MemberPlus dto) throws SQLException;
+	public void updatePlus2(MemberPlus dto) throws SQLException;
 	
 	public MemberPlus findById(String userId);
 	public MemberPlus findByMemberIdx(long memberIdx);
+	
+	public int checkFailureCount(String userId);
+	public void updateFailureCountReset(String userId) throws SQLException;
+	public void updateFailureCount(String userId) throws SQLException;
+	public void insertPlusState(MemberPlus dto) throws SQLException;
 	
 	public void insertAuthority(MemberPlus dto) throws SQLException;
 	public void updateAuthority(MemberPlus dto) throws SQLException;
