48fb5f67a34a6d1f90719225d22a4088c40df2d3
Kim Jeong Kyun <131667513+wjdrbs0433@users.noreply.github.com>
2023-12-19 22:15:18 +0900

    퀴즈 컨트롤러 - 정균

--- plus/src/main/java/com/fa/plus/pluszone/controller/PluszoneQuestionController.java
+++ plus/src/main/java/com/fa/plus/pluszone/controller/PluszoneQuestionController.java
@@ -1,14 +1,22 @@
 package com.fa.plus.pluszone.controller;
 
+import java.io.File;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.fa.plus.pluszone.domain.PluszoneQuesion;
+import com.fa.plus.admin.domain.NoticeManage;
+import com.fa.plus.domain.SessionInfo;
+import com.fa.plus.pluszone.domain.PluszoneAnswer;
+import com.fa.plus.pluszone.domain.PluszoneQuestion;
 import com.fa.plus.pluszone.service.PluszoneQuestionService;
 
 @Controller
@@ -20,9 +28,38 @@ public class PluszoneQuestionController {
 	
 	@GetMapping("list")
 	public String list(Model model) {
-		List<PluszoneQuesion> list = service.listQuesion();
+		List<PluszoneQuestion> list = service.listQuesion();
+		
+		model.addAttribute("list", list);
 		
 		return ".pluszone.question.list";
+	}
+	
+	@PostMapping("questionOk")
+	public String listSubmit(PluszoneAnswer dto, HttpSession session) throws Exception {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		try {
+			dto.setUserId(info.getUserId());
+			service.insertAnswer(dto);
+			
+		} catch (Exception e) {
+		}
+
+		return "redirect:/pluszone/question/complete";
+	}
+	
+	@GetMapping("article")
+		public String article(
+				@RequestParam String userId,
+				Model model) throws Exception{
+		
+		PluszoneAnswer dto = service.listAnswer(userId);
+		
+		model.addAttribute("dto", dto);
+		
+		return ".pluszone.question.article";
 	}
 	
 	
