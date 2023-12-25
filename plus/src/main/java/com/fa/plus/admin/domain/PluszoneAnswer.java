4e6c5fb64ff42392fa5b6b941e5043a14e8cdef7
HJ IM <74124891+hj0064@users.noreply.github.com>
2023-12-25 17:25:48 +0900

    플러스존 관리자 부분 진행 - 희정

---
+++ plus/src/main/java/com/fa/plus/admin/domain/PluszoneAnswer.java
@@ -0,0 +1,69 @@
+package com.fa.plus.admin.domain;
+
+import java.util.List;
+
+public class PluszoneAnswer {
+	private String userId;
+	private long pluszoneNum;
+	private long anszoneNum;
+	
+	private long num;
+	private String subject;
+	private String content;
+	
+	private long memberIdx;
+	
+	private List<Long> pluszoneNums;
+	
+	
+	public List<Long> getPluszoneNums() {
+		return pluszoneNums;
+	}
+	public void setPluszoneNums(List<Long> pluszoneNums) {
+		this.pluszoneNums = pluszoneNums;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public long getPluszoneNum() {
+		return pluszoneNum;
+	}
+	public void setPluszoneNum(long pluszoneNum) {
+		this.pluszoneNum = pluszoneNum;
+	}
+	public long getAnszoneNum() {
+		return anszoneNum;
+	}
+	public void setAnszoneNum(long anszoneNum) {
+		this.anszoneNum = anszoneNum;
+	}
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public long getMemberIdx() {
+		return memberIdx;
+	}
+	public void setMemberIdx(long memberIdx) {
+		this.memberIdx = memberIdx;
+	}
+	
+	
+}
---
+++ plus/src/main/java/com/fa/plus/admin/domain/PluszoneQuestion.java
@@ -0,0 +1,36 @@
+package com.fa.plus.admin.domain;
+
+public class PluszoneQuestion {
+	
+	private long num;
+	private String subject;
+	private long pluszoneNum;
+	private String content;
+	
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public long getPluszoneNum() {
+		return pluszoneNum;
+	}
+	public void setPluszoneNum(long pluszoneNum) {
+		this.pluszoneNum = pluszoneNum;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
