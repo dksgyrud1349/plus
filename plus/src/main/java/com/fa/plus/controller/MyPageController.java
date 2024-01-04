dd0c4a3c0c163dfda22b25aa3b0a4df6191d0895
userkae <148922328+userkae@users.noreply.github.com>
2024-01-04 17:27:39 +0900

    아은(마이페이지_메인 수정)

--- plus/src/main/java/com/fa/plus/controller/MyPageController.java
+++ plus/src/main/java/com/fa/plus/controller/MyPageController.java
@@ -1,14 +1,77 @@
 package com.fa.plus.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fa.plus.domain.BookingList;
+import com.fa.plus.domain.SessionInfo;
+import com.fa.plus.service.BookingListService;
 
 @Controller
 @RequestMapping(("/myPage/*"))
 public class MyPageController {
 	
+	@Autowired
+	private BookingListService bookingListSerivce;
+	
 	@RequestMapping(value = "main")
-	public String main() {
+	public String main(HttpSession session, Model model) {
+		
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		BookingList paramDto = new BookingList();
+		BookingList rtnDto = new BookingList();
+		List<BookingList> orderNumList = new ArrayList<BookingList>();
+		List<BookingList> rtnList = new ArrayList<BookingList>();
+		try {
+			
+			paramDto.setUserId(info.getUserId());
+			
+			// userId가 존재해야 if문 조건 참
+			if(paramDto.getUserId() != null && !"".equals(paramDto.getUserId())) {
+				// userId가 신청한 orderNum 전부 가져오기
+				orderNumList = bookingListSerivce.findByOrderNum(paramDto.getUserId());
+			}
+			
+			for(BookingList book : orderNumList) {
+				paramDto.setOrderNum(book.getOrderNum());
+				paramDto.setRnum(book.getRnum());
+				// 주문한 classNum 가져오기
+				rtnDto = bookingListSerivce.findByClassNum(book.getOrderNum());
+				
+				paramDto.setState(rtnDto.getState());
+				// classNum과 관련된 클래스 정보 가져오기
+				rtnDto = bookingListSerivce.classInfo(rtnDto.getClassNum());
+				
+				// state 조건문
+				if(paramDto.getState() == 0) {
+					paramDto.setMode("예약대기");
+				}else if(paramDto.getState() == 1) {
+					paramDto.setMode("예약확정");
+				}else {
+					paramDto.setMode("환불");
+				}
+				
+				rtnDto.setOrderNum(paramDto.getOrderNum());
+				rtnDto.setRnum(paramDto.getRnum());
+				rtnDto.setState(paramDto.getState());
+				rtnDto.setMode(paramDto.getMode());
+				
+				rtnList.add(rtnDto);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		model.addAttribute("rtnList", rtnList);
 		
 		return ".myPage.main";
 	}
